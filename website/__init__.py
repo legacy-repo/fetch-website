c3005989b712c1f07003f2af7d20822e69aec557
yjcyxky <yjcyxky@163.com>
2023-04-15 00:09:23 -0400

    First Commit.

---
+++ website/__init__.py
@@ -0,0 +1,22 @@
+from website import expression_atlas
+from website import gene_cards
+from website import sanger_cosmic
+
+websites = {
+    'expression_atlas': expression_atlas,
+    'gene_cards': gene_cards,
+    'sanger_cosmic': sanger_cosmic
+}
+
+blueprints = [
+    {
+        'name': 'gxa',
+        'url_prefix': '/gxa',
+        'blueprint': expression_atlas.subroute_blueprint
+    },
+    {
+        'name': 'sanger_cosmic',
+        'url_prefix': '/sanger_cosmic',
+        'blueprint': sanger_cosmic.subroute_blueprint
+    }
+]
